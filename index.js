4073602241b89fc53226c38bc3b333e567c90fdb
roukiaSalahi <roqaia.sa@hotmail.com>
2020-09-07 01:27:25 +0300

    done

---
+++ index.js
@@ -0,0 +1,10 @@
+#!/usr/bin/env node
+
+'use strict';
+
+const Input = require('./lib/input.js');
+const Notes = require('./lib/notes.js');
+
+const options = new Input();
+const note=new Notes(options)
+note.execute(options)
